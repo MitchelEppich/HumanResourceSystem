e707358f01274529776770b03e32472f7342838c
Mitchel Eppich <mitchel.eppich.work@gmail.com>
2018-12-18 08:29:33 -0800

    small fixes to allow for the subscriptions of users to external clients

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,17 +4,12 @@ let resolvers = require("../../data/resolvers");
 
 let router = express.Router();
 
-router.post("/", getUsers);
+router.get("/", getUsers);
 router.post("/verify", verifyUser);
 router.post("/update", updateUser);
 
 async function getUsers(req, res) {
-  let _post = req.body;
-
   let _users = await resolvers.Query.allUsers(null, {});
-  _users = _users.map(user => {
-    return inferPermissions(user.toObject(), _post.CLIENT_ACR);
-  });
   res.send(_users);
 }
 
@@ -25,7 +20,6 @@ async function verifyUser(req, res) {
       ..._post
     }
   });
-  _user = inferPermissions(_user, _post.CLIENT_ACR);
   res.send(_user);
 }
 
@@ -36,25 +30,7 @@ async function updateUser(req, res) {
       ..._post
     }
   });
-  _user = inferPermissions(_user, _post.CLIENT_ACR);
   res.send(_user);
-}
-
-function inferPermissions(user, CLIENT_ACR) {
-  let _new = user;
-
-  let _permissions =
-    _new.permissions != null && _new.permissions.length != 0
-      ? _new.permissions
-          .filter(a => {
-            if (a.includes(CLIENT_ACR)) return true;
-            return false;
-          })[0]
-          .split(":")
-      : [CLIENT_ACR, 0, 0];
-  _new.admin = _permissions[1] == "1";
-  _new.locked = _permissions[2] == "1";
-  return _new;
 }
 
 module.exports = router;
